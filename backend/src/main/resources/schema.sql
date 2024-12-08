7180f59572c2909f10a866c74763beb5a0bdc73e
ahmedyoussefg <108885178+ahmedyoussefg@users.noreply.github.com>
2024-12-09 00:58:24 +0200

    Add schema.sql file with ParkingLot and ParkingSpot

---
+++ backend/src/main/resources/schema.sql
@@ -0,0 +1,19 @@
+CREATE TABLE IF NOT EXISTS ParkingLot (
+    id bigint NOT NULL AUTO_INCREMENT,
+    location varchar(1024) NOT NULL,
+    capacity int NOT NULL,
+    time_limit decimal NOT NULL,
+    automatic_release_time decimal NOT NULL,
+    not_showing_up_penalty decimal NOT NULL,
+    over_time_scale decimal NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS ParkingSpot (
+    id bigint NOT NULL,
+    lot_id bigint NOT NULL,
+    cost decimal NOT NULL,
+    current_status varchar(255) NOT NULL,
+    type varchar(255) NOT NULL,
+    PRIMARY KEY (id, lot_id)
+);
