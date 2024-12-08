cbcb9ca4d63b48b345a4ef6c552e0b9153644768
Ab00dMD <abdalrhmanmohamed94@gmail.com>
2024-12-08 23:59:51 +0200

    Adding signup page

--- frontend/src/constants/Constants.jsx
+++ frontend/src/constants/Constants.jsx
@@ -1,2 +1,3 @@
 export const BACKEND_BASE = "http://localhost:8080"
 export const LOGIN_API = "/login"
+export const REGISTER_API = "/register"
---
+++ frontend/src/pages/signup/Signup.jsx
@@ -0,0 +1,134 @@
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { registerUser } from "../../APIs/userRegister";
+import Password from '../../components/password/Password';
+import '../../styles/form.css'
+
+function LoginForm(){
+
+    const [username, setUsername] = useState("");
+    const [email, setEmail] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
+    const [password, setPassword] = useState("");
+    const [usernameError, setUsernameError] = useState("");
+    const [emailError, setEmailError] = useState("");
+    const [passwordError, setPasswordError] = useState("");
+    const [errorMessage, setErrorMessage] = useState("");
+
+    const navigate = useNavigate();
+
+    function isValidEmail(email) {
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return emailRegex.test(email);
+    }
+
+    const handleSubmit = async () => {
+        setUsernameError("");
+        setPasswordError("");
+        setErrorMessage("");
+
+        if(username === ''){
+            setUsernameError("Username is required!");
+            return;
+        }
+
+        if(username.length < 2){
+            setUsernameError("The username must be at least 3 characters");
+            return;
+        }
+
+        if(email === ''){
+            setEmailError("Email is required!");
+            return;
+        }
+
+        if(!isValidEmail(email)){
+            setEmailError("Enter a valid email");
+            return;
+        }
+
+        if(password === ''){
+            setPasswordError("Password is required!");
+            return;
+        }
+
+        if(password.length < 7){
+            setPasswordError("The password must be at least 8 characters");
+            return;
+        }
+
+        if (!(password === confirmPassword)) {
+            setPasswordError('Passwords do not match');
+            return;
+        }
+
+        const userData = {
+            username: username,
+            email: email,
+            password: password,
+        };
+
+
+        try {
+            const response = await registerUser(userData); 
+            console.log('User registered successfully:', response.data);
+            navigate('/');
+
+          } catch (error) {
+            console.error('Registration failed:', error.response ? error.response.data : error.message);
+            setErrorMessage(error.response.data);
+          }
+       
+    };
+
+    return(
+        <div className="wrapper">
+            <form action="">
+                <h1>Signup</h1>
+
+                <div className="input-box">
+                    <input type="text" 
+                        placeholder="username"
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                    />
+                    <p className="error">{usernameError}</p>
+                </div>
+
+                <div className="input-box">
+                    <input type="email" 
+                        placeholder="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                    <p className="error">{emailError}</p>
+                </div>
+
+                <div className="input-box">
+                <Password
+                    value={password}
+                    setValue={setPassword}
+                    placeholder="password"
+                />
+                    <p className="error">{passwordError}</p>
+                </div>
+
+                <div className="input-box">
+                <Password
+                    value={confirmPassword}
+                    setValue={setConfirmPassword}
+                    placeholder="confirm password"
+                />
+                </div>
+                
+                <p className="error">{errorMessage}</p>
+                <button type="button" className="btn" onClick={handleSubmit}>Create Account</button>
+                <div className="register">
+                    <p>Already have an account? <Link to="/login">login</Link></p>
+                </div>
+            </form>
+        </div>
+    )
+}
+
+export default LoginForm;
