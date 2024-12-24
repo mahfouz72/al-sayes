b38e86f1c5d01a7e03b09e8436e49fd81e9fe0c4
AbdElRahman Osama <bodyusama207@gmail.com>
2024-12-25 00:46:30 +0200

    Directing to login

--- frontend/src/components/auth/SignupForm.jsx
+++ frontend/src/components/auth/SignupForm.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useForm } from "react-hook-form";
 
 export default function SignupForm() {
@@ -22,8 +22,17 @@ export default function SignupForm() {
             <div className="max-w-md w-full space-y-8 bg-white p-8 rounded-xl shadow-2xl">
                 <div>
                     <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-                        Create your account
+                        Create Your Account
                     </h2>
+                    <p className="mt-2 text-center text-sm text-gray-600">
+                        Or Already have an account?{" "}
+                        <Link
+                            to="/login"
+                            className="font-medium text-blue-600 hover:text-blue-500"
+                        >
+                            Sign in
+                        </Link>
+                    </p>
                 </div>
                 <form
                     className="mt-8 space-y-6"
