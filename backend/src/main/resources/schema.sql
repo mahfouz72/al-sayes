c9881fdad00593df274834144c68f4f01d921a35
ahmedyoussefg <108885178+ahmedyoussefg@users.noreply.github.com>
2024-12-26 19:46:25 +0200

    Add on delete cascade in schema

--- backend/src/main/resources/schema.sql
+++ backend/src/main/resources/schema.sql
@@ -18,7 +18,7 @@ CREATE TABLE IF NOT EXISTS ParkingLot (
     not_showing_up_penalty decimal NOT NULL,
     over_time_scale decimal NOT NULL,
     PRIMARY KEY (id),
-    FOREIGN KEY (managed_by) REFERENCES Account(id)
+    FOREIGN KEY (managed_by) REFERENCES Account(id) ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS ParkingSpot (
@@ -27,7 +27,15 @@ CREATE TABLE IF NOT EXISTS ParkingSpot (
     cost decimal NOT NULL,
     current_status varchar(255) NOT NULL,
     type varchar(255) NOT NULL,
-    PRIMARY KEY (id, lot_id)
+    PRIMARY KEY (id, lot_id),
+    FOREIGN KEY (lot_id) REFERENCES ParkingLot(id)
+);
+
+CREATE TABLE IF NOT EXISTS Driver (
+    account_id BIGINT NOT NULL,
+    license_plate VARCHAR(255) NOT NULL,
+    PRIMARY KEY (account_id),
+    FOREIGN KEY (account_id) REFERENCES Account(id) ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS Reservation (
@@ -40,13 +48,8 @@ CREATE TABLE IF NOT EXISTS Reservation (
     status varchar(255) NOT NULL,
     violation_duration decimal NOT NULL,
     penalty decimal NOT NULL,
-    PRIMARY KEY (driver_id, spot_id, lot_id, start_time)
-);
-
-CREATE TABLE IF NOT EXISTS Driver (
-    account_id BIGINT NOT NULL,
-    license_plate VARCHAR(255) NOT NULL,
-    PRIMARY KEY (account_id),
-    FOREIGN KEY (account_id) REFERENCES Account(id) ON DELETE CASCADE
+    PRIMARY KEY (driver_id, spot_id, lot_id, start_time),
+    FOREIGN KEY(driver_id) REFERENCES Driver(account_id) ON DELETE CASCADE,
+    FOREIGN KEY(spot_id, lot_id) REFERENCES ParkingSpot(id, lot_id) ON DELETE CASCADE
 );
 
