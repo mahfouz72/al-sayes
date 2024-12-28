faee49c91b4db042bef8f1184f61f23807a286bf
Ab00dMD <abdalrhmanmohamed94@gmail.com>
2024-12-28 08:14:28 +0200

    Updating download buttons

--- frontend/src/components/admin/TopLotsReport.jsx
+++ frontend/src/components/admin/TopLotsReport.jsx
@@ -171,7 +171,7 @@ export default function TopLotsReport() {
                     onClick={handleDownloadUsers}
                     className="py-2 px-4 bg-blue-500 text-white font-medium text-sm rounded-lg hover:bg-blue-400 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 transition-all"
                     >
-                    Download Parking Lots
+                    Download parking lots
                   </button>
                 </div>
 
--- frontend/src/components/admin/TopUsersReport.jsx
+++ frontend/src/components/admin/TopUsersReport.jsx
@@ -101,7 +101,7 @@ export default function TopUsersReport() {
             onClick={handleDownloadUsers}
             className="py-2 px-4 bg-blue-500 text-white font-medium text-sm rounded-lg hover:bg-blue-400 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 transition-all"
             >
-            Download
+            Download top users
           </button>
         </div>
       <div className="mb-6">
